00b245743aab5ee4ab4c746f8cea15495b953732
Andy Smith <charles.a.smith5@gmail.com>
2021-01-14 10:31:44 -0500

    more user data updates

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export AWS_CODEGURU_PROFILER_GROUP_NAME=python-demo
+export AWS_CODEGURU_PROFILER_GROUP_NAME=python-demo-app
 export AWS_CODEGURU_PROFILER_TARGET_REGION=us-east-1
 
 python3 -m codeguru_profiler_agent producer.py
